9ee3fe063d85ceabe9f62877c87fe37f152eeca5
Matthew Talbert <43828089+mattonri@users.noreply.github.com>
2024-06-01 12:25:26 -0600

    fixed the reviews

--- blog/blog.js
+++ blog/blog.js
@@ -1,5 +1,5 @@
 import { articles } from './articles_data.js';
-
+const reviews = articles.reviews;
 function insertArticles(articles)
 {
     const articleContainer = document.getElementById('article-gallery')
@@ -20,13 +20,23 @@ function constructArticle(article)
         <p class="book-summary">${article.description}<a href="#">Read More...</a></p>
     </div>
     <img src="${article.imgSrc}" alt="${article.imgAlt}">
-    <div class="book-reviews">
-        <p><strong>User:</strong>${article.reviews.user} - ${article.reviews.stars}<strong>Date:</strong>${article.reviews.date}<br>
-        <em>${article.reviews.comment}</em></p>
-    </div>
+    <div class="book-reviews">` + constructReviews(article)+ `</div>
 </article>`;
 }
 
+function constructReviews(article) {
+    let reviewsString = ``;
+    if (article.reviews && article.reviews.length > 0) {
+        article.reviews.forEach(review => {
+            reviewsString += constructReview(review);
+        });
+    }
+    return reviewsString;
+}
+
+function constructReview(review) {
+    return `<p><strong>User:</strong>${review.user} - ${review.stars}<strong>Date:</strong>${review.date}<br><em>${review.comment}</em></p>`;
+}
 function insertArticlesOnPageLoad() {
     window.addEventListener('load', () => {
         insertArticles(articles);
